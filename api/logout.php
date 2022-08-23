0c4382eccfd23002b916ba701c1b8aad30b4214b
JimWu <a0283137@gmail.com>
2022-08-23 08:29:57 +0800

    完成登出功能及相關的連結顯示

---
+++ api/logout.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+//unset($_SESSION[$_GET['table']]);
+
+switch($_GET['table']){
+    case 'mem':
+        unset($_SESSION['mem']);
+    break;
+    case 'admin':
+        unset($_SESSION['admin']);
+    break;
+} 
+
+?>
